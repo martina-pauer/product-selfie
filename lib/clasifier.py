14d0ed2150c88d0652513ce766e74bd4c27a70fb
Martina Pauer <martinapauer38@gmail.com>
2026-03-13 18:14:43 +0000

    Fix Indentation

--- lib/clasifier.py
+++ lib/clasifier.py
@@ -2,13 +2,13 @@ class DatagramGenerator:
   
   def __init__(self):
     '''
-      Generate the HTML file output
-      with product variants and how
-      much is used each one.
+        Generate the HTML file output 
+        with product variants and how
+        much is used each one.
 
-      Strcture each data about image, categories
-      and counting mainly.
-   '''
+        Strcture each data about image, categories
+        and counting mainly.
+    '''
     self.categories: list[str] = []
     # Key Category and Count of Same category images
     self.poll: dict[str, int] = {}
@@ -49,43 +49,44 @@ class DatagramGenerator:
     return images
 
   def write_file(self, name: str):
-    '''
-      Write the HTML output
-    '''
-    prefix: str = './'
-    # Prepare and proceed
-    try:
-      # Delete if exist old files
-      import os
-      os.system(f'rm {prefix}/{name}.html')
-      del prefix
-    except:
-      pass
-    # Delete uneed module for free out memory
-    del os
-    # Prepare content
-    content: str = '<!DOCTYPE html>\n\t'
-    # Page Metadata
-    content += '<head>\n\t\t'
-    content += '</head>\n\t'
-    # Visible Part
-    content += '<body>\n\t\t'
-    for category in self.poll.keys():
-      # Count the images in each category
-      content += '<div style = "text-align: center; padding: 0.5em; border: 2px solid black; border-radius: 0.5em; background: #2535cf; color: #efefef; font-weight: 840;">\n\t\t\t'
-      content += f'{category}<span style = "text-align: center; margin: 0.5 padding: 0.5em; border: 2px solid black; background: #efefef; color: black; font-weight: 840;">{self.poll[category]}</span\n'
-      content += '\t\t</div>\n\t\t'
-    content += '</body>\n'
-    content += '<html>'
-    # Create and write html
-    output = open(name, 'x')
-    output.write(content)
-    del content
-    output.close()
-    del output
-
+      '''
+        Write the HTML output
+      '''
+      prefix: str = './'
+      # Prepare and proceed
+      try:
+        # Delete if exist old files
+        import os
+        os.system(f'rm {prefix}/{name}.html')
+        del prefix
+      except:
+        pass
+      # Delete uneed module for free out memory
+      del os
+      # Prepare content
+      content: str = '<!DOCTYPE html>\n\t'
+      # Page Metadata
+      content += '<head>\n\t\t'
+      content += '</head>\n\t'
+      # Visible Part
+      content += '<body>\n\t\t'
+      for category in self.poll.keys():
+        # Count the images in each category
+        content += '<div style = "text-align: center; padding: 0.5em; border: 2px solid black; border-radius: 0.5em; background: #2535cf; color: #efefef; font-weight: 840;">\n\t\t\t'
+        content += f'{category}<span style = "text-align: center; margin: 0.5 padding: 0.5em; border: 2px solid black; background: #efefef; color: black; font-weight: 840;">{self.poll[category]}</span\n'
+        content += '\t\t</div>\n\t\t'
+      # HTML ending  
+      content += '</body>\n'
+      content += '<html>'
+      # Create and write html
+      output = open(name, 'x')
+      output.write(content)
+      del content
+      output.close()
+      del output
+      
   def get_path(self, category: str) -> str:
-    return self.folders_paths[category]
+      return self.folders_paths[category]
 
 class  ImageClasify:
   def __init__(self):
@@ -109,21 +110,21 @@ class  ImageClasify:
     self.name = name
 
   def set_category(self, category: str):
-      '''
-        Define a category where better fits the
-        image.
-      '''
-      self.category = category
+    '''
+      Define a category where better fits 
+      the image.
+    '''
+    self.category = category
 
   def get_folder(self) -> str:
-      '''
-        Path to the folder with the image
-        is initially.
-      '''
-      return self.folder_path
+    '''
+      Path to the folder with the image 
+      is initially.
+    '''
+    return self.folder_path
 
   def get_category(self) -> str:
-      '''
-        Category for clasiy the image.
-      '''       
-      return self.category
+    '''
+      Category for clasiy the image.
+    '''
+    return self.category
